1f9d7c0d7c6b6bca5369de1c524fc030e2f05a42
sahassan012 <sahassan012@gmail.com>
2021-04-17 21:52:22 -0500

    Add methods for adding and removing count from Nurse Schedule Manager table as schedules are added/removed

--- website/services.py
+++ website/services.py
@@ -1,10 +1,14 @@
-from .models import Nurse_Schedule
+from .models import Nurse_Schedule, Nurse_Schedule_Manager
 from flask import flash
-from datetime import datetime
+from datetime import datetime, timedelta
+from . import db
 
 def check_schedules_for_conflict(schedules, start_time, end_time, schedule_id=None, updating=False):
     if end_time <= start_time:
         flash("The given start time was after the end time.", category='error')
+        return 0
+    elif start_time < datetime.today() or end_time < datetime.today():
+        flash("Start and end dates must be in the future.", category='error')
         return 0
     for schedule in schedules:
         if schedule.start_time == start_time and schedule.end_time == end_time:
@@ -45,3 +49,26 @@ def str_to_datetime_v2(date_str):
         return datetime.strptime(date_str, "%Y-%m-%d %I:%M %p")
     else:
         return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+
+def add_schedule_count(start_time, end_time):
+    while(start_time <= end_time):
+        start_datehour = start_time.strftime("%Y-%m-%d %H:00")
+        time_slot = Nurse_Schedule_Manager.query.filter_by(timestamp = start_datehour).first()
+        if time_slot:
+            time_slot.count += 1
+        else:
+            new_time_slot = Nurse_Schedule_Manager(timestamp=start_datehour, count=1)
+            db.session.add(new_time_slot)
+        db.session.commit()
+        start_time += timedelta(hours=1)
+
+def remove_schedule_count(start_time, end_time):
+    while(start_time <= end_time):
+        start_datehour = start_time.strftime("%Y-%m-%d %H:00")
+        time_slot = Nurse_Schedule_Manager.query.filter_by(timestamp = start_datehour).first()
+        if time_slot:
+            time_slot.count -= 1
+        if (time_slot.count <= 0):
+            db.session.delete(time_slot)
+        db.session.commit()
+        start_time += timedelta(hours=1)
